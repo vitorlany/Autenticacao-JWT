ee6ab57f7375b4a775d9da0912c00e713bc6ca23
Vitor Lany <vitorlany99@gmail.com>
2022-08-12 18:59:12 -0300

    poucas modificacoes

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const SECRET = "TESTETESTE" // passar isso para .env
 
 // nodemon index.js
 
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next();
+})
+
 app.use(express.json())
 
 console.log("Servidor iniciado!")
 
-app.get("/", (req, res, next) => {
+app.post("/", (req, res, next) => {
     const CALLBACK = req.headers["authorization"]
     if (CALLBACK) {
         try {
@@ -23,12 +30,17 @@ app.get("/", (req, res, next) => {
     }
 })
 
-app.get("/login", (req, res, next) => {
-    /*// Aqui faz a autenticacao no banco de dados
+app.post("/login", (req, res, next) => {
+    // Aqui faz a autenticacao no banco de dados
     const USUARIO = req.body.usuario
-    const SENHA = req.body.senha*/
-    let resposta = jwt.sign({"userID":1}, SECRET, {expiresIn: 3600}) // Mudar o id pelo banco
-    res.status(200).json({"token":resposta})
+    const SENHA = req.body.senha
+    console.log(USUARIO, SENHA, req.body)
+    if (USUARIO == "Vitor" && SENHA == "123") {
+        let resposta = jwt.sign({"userID":1}, SECRET, {expiresIn: 3600}) // Mudar o id pelo banco
+        res.status(200).json({"token":resposta})
+    } else {
+        res.status(203).json({"err":"Usuário inválido"})
+    }
 })
 
 app.listen(3000)
