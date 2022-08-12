81e583cb028d34e171124e00d839521e21c22fe6
Vitor Lany <vitorlany99@gmail.com>
2022-08-12 16:02:33 -0300

    Autenticacao

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express")
+const jwt = require("jsonwebtoken")
 const app = express()
+const SECRET = "TESTETESTE" // passar isso para .env
 
 app.use(express.json())
 
 console.log("Servidor iniciado!")
 
-app.get("/", (req, res, next) => {
+app.get("/err", (req, res, next) => {
     console.log(req.body)
     res.status(404).json({"err":"Usuário não autenticado!"})
+})
+app.get("/", (req, res, next) => {
+    console.log(req.body)
+    let resposta = jwt.sign({"userID":1}, SECRET)
+    res.status(200).json({"token":resposta})
 })
 
 app.listen(3000)
