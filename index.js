ca02d71e417e1d166ce5194bb67d1fc87e866ffc
Vitor Lany <vitorlany99@gmail.com>
2022-08-12 17:02:36 -0300

    Data de expiração

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use(express.json())
 console.log("Servidor iniciado!")
 
 app.get("/", (req, res, next) => {
-    let callback = req.headers['authorization']
-    if (callback) {
+    const CALLBACK = req.headers["authorization"]
+    if (CALLBACK) {
         try {
-            jwt.verify(callback, SECRET)
+            jwt.verify(CALLBACK, SECRET)
         }catch (err) {
             res.status(203).json({"err":"Usuário não autenticado!"})
         }
@@ -27,7 +27,7 @@ app.get("/login", (req, res, next) => {
     /*// Aqui faz a autenticacao no banco de dados
     const USUARIO = req.body.usuario
     const SENHA = req.body.senha*/
-    let resposta = jwt.sign({"userID":1}, SECRET) // Mudar o id pelo banco
+    let resposta = jwt.sign({"userID":1}, SECRET, {expiresIn: 3600}) // Mudar o id pelo banco
     res.status(200).json({"token":resposta})
 })
 
